8d26f1b1627c81c81024bf726a358d1b1076858d
Saim Rafi <saimrafi123@gmail.com>
2024-04-17 23:17:04 +0530

    episode-3 "Functional component , jsx , babel" done

--- App.js
+++ App.js
@@ -2,23 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const heading = React.createElement(
-    "h1", 
-    {id:"heading"}, 
-    "Hello World From ReactJS!");
+//React Functional Component
+const Heading1 = ()=> (
+  <h1 className="heading1">
+    This is a heading!
+  </h1>
+);
+ 
+const number=1000;//we can use js inside the functional component using {}
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child"},
-        [React.createElement("h1",{},"This is an h1 tag"),React.createElement("h1",{},"This is an h2 tag")]))
 
-// console.log(heading);
-console.log(parent);
+//Conponent composition
+const Heading2 = () => (
+    <div id="container">
+        {Heading1()}
+        <Heading1 />
+
+        {number}
+
+        <h1 className="heading 2">
+            This is another heading!
+        </h1>
+    </div>
+
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-root.render(parent);
+//jsx- React.createElement => ReactElement-Js Object => HTMLelement(render)
+
+// const jsxheading = <h1 className="head">This is a heading from JSX⚡️</h1>;
+
+root.render(<Heading2 />);
+
